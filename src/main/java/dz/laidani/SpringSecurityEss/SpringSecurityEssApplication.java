5c3c0d3663afe9b3193c26c2a9158b4c63052898
Mohamed LAIDANI <laidani.it@gmail.com>
2020-07-11 19:54:29 +0100

    First commit

---
+++ src/main/java/dz/laidani/SpringSecurityEss/SpringSecurityEssApplication.java
@@ -0,0 +1,13 @@
+package dz.laidani.SpringSecurityEss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityEssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityEssApplication.class, args);
+	}
+
+}
